84aa3a04a4eb9e80101a7483382f40c4e3c97405
Darturium <darturium@gmail.com>
2014-02-26 00:18:44 -0500

    Created and added Program 1 for Chapter 2in its own directory.

---
+++ chapter_2/chapter2_program1.rb
@@ -0,0 +1,10 @@
+
+hours_year = 24 * 365
+minutes_decade = 60 * hours_year * 10
+age_in_seconds = 60 * 60 * hours_year * 27
+
+puts "There are #{hours_year} hours in a year, #{minutes_decade} minutes in a decade, and my age in seconds is #{age_in_seconds} seconds."
+
+author_age =  1160000000 / 60 /60 / 24/ 365
+
+puts "If the author is 1,160 seconds old, then he is is #{author_age} years old."
