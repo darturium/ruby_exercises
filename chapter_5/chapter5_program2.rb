5ac9259c80e5be9707bcaba0f1b65fdec99f62f9
Darturium <darturium@gmail.com>
2014-02-27 01:07:38 -0500

    Created chapter5_program2.rb

---
+++ chapter_5/chapter5_program2.rb
@@ -0,0 +1,4 @@
+print "Hi, please enter your favorite number (please use an integer): "
+fav_num = gets.chomp
+
+puts "So your favorite number is #{fav_num} huh? May I suggest #{fav_num.to_i + 1} to be your bigger, better favorite number?" 
