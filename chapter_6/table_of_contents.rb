b2b0278b11548e32750a29888e157091c7bda5ea
Darturium <darturium@gmail.com>
2014-02-27 22:20:08 -0500

    Added table_of_contents.rb & 99_bottles.rb

---
+++ chapter_6/table_of_contents.rb
@@ -0,0 +1,10 @@
+line_width = 30
+
+puts("Table of Contents".center(60))
+print("Chapter 1: Getting Started".ljust(line_width))
+puts("page  1".rjust(line_width))
+print("Chapter 2: Numbers".ljust(line_width))
+puts("page  9".rjust(line_width))
+print("Chapter 3: Letters".ljust(line_width))
+puts("page 13".rjust(line_width))
+puts "\n"
---
+++ chapter_7/99_bottles.rb
@@ -0,0 +1,8 @@
+num = 99
+
+while num > 0
+  puts "#{num} bottles of beer on the wall, #{99} bottles of beer."
+  puts "Take one down, pass it around, #{num - 1} bottles of beer on the wall..."
+  print "\n"
+  num -= 1
+end
