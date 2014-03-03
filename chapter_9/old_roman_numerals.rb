0c4768d5154e1d194132655c234284b849d4f7f1
Darturium <darturium@gmail.com>
2014-03-02 21:01:55 -0500

    Added sorting.rb

--- chapter_9/old_roman_numerals.rb
+++ chapter_9/old_roman_numerals.rb
@@ -14,18 +14,28 @@ def get_units(num)
   num % 10
 end 
 
-print "Enter number: "
+puts "INTEGER TO OLD ROMAN NUMERAL CONVERSION".center(60)
+print "Enter number or 'bye' to exit: "
 num = gets.chomp
 
-num = num.to_i
+while true
+  unless num == "bye"
+    num = num.to_i
 
-m = "M" * get_thousands(num)
-d = "D" * ( get_hundreds(num) / 5 )
-c = "C" * ( get_hundreds(num) % 5 )
-l = "L" * ( get_tens(num) / 5 )
-x = "X" * ( get_tens(num) % 5 )
-v = "V" * ( get_units(num) / 5 )
-i = "I" * ( get_units(num) % 5)
+    m = "M" * get_thousands(num)
+    d = "D" * ( get_hundreds(num) / 5 )
+    c = "C" * ( get_hundreds(num) % 5 )
+    l = "L" * ( get_tens(num) / 5 )
+    x = "X" * ( get_tens(num) % 5 )
+    v = "V" * ( get_units(num) / 5 )
+    i = "I" * ( get_units(num) % 5)
 
-puts m + d + c + l + x + v + i
+    puts m + d + c + l + x + v + i
 
+  else
+    break
+  end
+
+  print "Enter number or 'bye' to exit: "
+  num = gets.chomp  
+end
