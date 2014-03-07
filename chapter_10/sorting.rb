ae84822260031c9a85d07d136aff32c32b580033
Darturium <darturium@gmail.com>
2014-03-07 01:21:37 -0500

    Completed code for sorting.rb program

--- chapter_10/sorting.rb
+++ chapter_10/sorting.rb
@@ -1,10 +1,32 @@
+require "pry"
 
 def sort some_array
   recursive_sort some_array, []
 end
 
 def recursive_sort unsorted_array, sorted_array
-  unsorted_array.each {
-    
-  }
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+
+  smallest = unsorted_array.pop
+  still_unsorted = []
+
+  unsorted_array.each do |element|
+    if smallest > element
+      still_unsorted.push smallest
+      smallest = element
+    else
+      still_unsorted.push element
+    end
+  end
+
+  sorted_array.push smallest
+  recursive_sort still_unsorted, sorted_array
 end
+
+to_sort = ["David", "Daniel", "Jacob", "Isaac", "Abraham", "Paul"]
+
+binding.pry
+puts sort(to_sort)
+
